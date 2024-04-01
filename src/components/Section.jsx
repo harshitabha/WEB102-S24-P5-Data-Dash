9e0bb7414d634b2de2adbfce59bb89480987d399
Harshita B <honeyblue2604@gmail.com>
2024-03-31 21:02:57 -0700

    css

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,7 +7,7 @@ const Section = (props) => {
         <div className="section-container">
             <h2 className="section-header">{props.header}</h2>
             <hr />
-            <ul>
+            <ul className="section-list">
                 {props.content ? props.content.map((item, index) => {
                    return <li key={`${props.keyInfo}-${index}`} className="table-elem">{item ? item : "N/A"}</li>
                 }) : null}
